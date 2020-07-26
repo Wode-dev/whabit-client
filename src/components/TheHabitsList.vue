9aadbf6f4c2c5883d1bed0d35a784de4f6a5448f
Albertto Castro <albertto.programmer@gmail.com>
2020-07-26 14:46:35 -0300

    Página de hábito único

--- src/components/TheHabitsList.vue
+++ src/components/TheHabitsList.vue
@@ -1,7 +1,7 @@
 <template>
     <b-list-group>
         <b-list-group-item v-for="habit in habits" v-bind:key="habit.name">
-            <span @click="goToHabitPage">{{habit.name}}</span>
+            <span @click="goToHabitPage(habit)">{{habit.name}}</span>
             <b-form-checkbox class="float-right"></b-form-checkbox>
         </b-list-group-item>
     </b-list-group>
@@ -13,10 +13,12 @@ export default {
         return {
             habits: [
                 {
+                    id: 1,
                     name: "Habito 1",
                     completed: [Date.now()],
                 },
                 {
+                    id: 2,
                     name: "Habito 2",
                     completed: ["Oi"],
                 },
@@ -24,8 +26,13 @@ export default {
         };
     },
     methods: {
-        goToHabitPage() {
-            this.$root.$options.router.push({ name: "habit" });
+        goToHabitPage(habit) {
+            this.$root.$options.router.push({
+                name: "habit",
+                params: {
+                    habit: habit,
+                },
+            });
         },
     },
 };
---
+++ src/views/habit/Index.vue
@@ -0,0 +1,24 @@
+<template>
+    <main-layout>
+        <b-container class="pt-3">
+            <p class="h1">{{habit.name}}</p>
+            <b-button :to="{name: 'Home'}">Voltar</b-button>
+        </b-container>
+    </main-layout>
+</template>
+
+<script>
+import MainLayout from "../../layouts/MainLayout";
+// import Habit from "../../core/models/Habit";
+export default {
+    data() {
+        return {
+            habit: { name: "Undefined" },
+        };
+    },
+    components: { MainLayout },
+    created() {
+        this.habit = this.$route.params.habit;
+    },
+};
+</script>
