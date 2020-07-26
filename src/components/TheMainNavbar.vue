981bff5ec22126e14c6c5b63547943644b6e5fe8
Albertto Castro <albertto.programmer@gmail.com>
2020-07-26 11:46:38 -0300

    Configura navbar

---
+++ src/components/TheMainNavbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <b-navbar toggleable="sm" type="light" variant="light">
+        <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+
+        <b-navbar-brand>Whabit</b-navbar-brand>
+
+        <b-collapse id="nav-text-collapse" is-nav>
+            <b-navbar-nav>
+                <b-nav-item :to="{name: 'Home'}">Inicio</b-nav-item>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,20 +1,15 @@
 <template>
     <div>
-        <b-navbar toggleable="sm" type="light" variant="light">
-            <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
-
-            <b-navbar-brand>Whabit</b-navbar-brand>
-
-            <b-collapse id="nav-text-collapse" is-nav>
-                <b-navbar-nav>
-                    <b-nav-text>Navbar text</b-nav-text>
-                </b-navbar-nav>
-            </b-collapse>
-        </b-navbar>
+        <main-navbar></main-navbar>
         <slot></slot>
     </div>
 </template>
 
 <script>
-export default {};
+import MainNavbar from "../components/TheMainNavbar";
+export default {
+    components: {
+        MainNavbar,
+    },
+};
 </script>
