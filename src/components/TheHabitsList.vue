05ccd2c52559850e033927f924062512d4aded86
Albertto Castro <albertto.programmer@gmail.com>
2020-08-15 19:23:28 -0300

    Insere animação de carregamento para habitos

--- src/components/TheHabitsList.vue
+++ src/components/TheHabitsList.vue
@@ -1,24 +1,15 @@
 <template>
     <div>
-        <b-list-group v-if="habits.length > 0">
+        <b-list-group>
             <b-list-group-item v-for="habit in habits" v-bind:key="habit.id">
                 <span @click="goToHabitPage(habit)">{{habit.name}}</span>
             </b-list-group-item>
         </b-list-group>
-        <div v-else>
-            <p>Você ainda não tem nenhum hábito :( Crie algum hábito agora!</p>
-        </div>
     </div>
 </template>
 
 <script>
-import Habit from "../core/models/Habit";
 export default {
-    data() {
-        return {
-            habits: [],
-        };
-    },
     methods: {
         goToHabitPage(habit) {
             this.$root.$options.router.push({
@@ -28,26 +19,9 @@ export default {
                 },
             });
         },
-        getAllHabitsFromAPI() {
-            // Get all habits from API
-            let component = this;
-            Habit.getAllFromAPI()
-                .then((response) => {
-                    component.habits = response.data.data;
-                })
-                .catch((error) => {
-                    console.log({ error });
-                });
-        },
     },
-    beforeMount() {
-        this.getAllHabitsFromAPI();
-
-        // Add event listener para
-        let comp = this;
-        this.$root.$on("new-habit-created", () => {
-            comp.getAllHabitsFromAPI();
-        });
+    props: {
+        habits: Array,
     },
 };
 </script>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -4,7 +4,18 @@
             <div class="text-right mb-2">
                 <b-button v-b-modal.new-habit>Novo hábito</b-button>
             </div>
-            <habits-list class="mb-2"></habits-list>
+            <div v-if="loading.habits" class="d-flex flex-column align-items-center">
+                <self-building-square-spinner
+                    :animation-duration="6000"
+                    :size="40"
+                    color="#000000"
+                />
+                <span>Carregando...</span>
+            </div>
+            <habits-list :habits="habits" v-else-if="habits.length > 0" class="mb-2"></habits-list>
+            <div v-else>
+                <p>Você ainda não tem nenhum hábito :( Crie algum hábito agora!</p>
+            </div>
         </b-container>
         <new-habit-modal></new-habit-modal>
     </MainLayout>
@@ -14,12 +25,49 @@
 import MainLayout from "../layouts/MainLayout";
 import HabitsList from "../components/TheHabitsList";
 import NewHabitModal from "./components/NewHabitModalComponent";
+import { SelfBuildingSquareSpinner } from "epic-spinners";
+import Habit from "../core/models/Habit";
 export default {
     name: "Home",
     components: {
         MainLayout,
         HabitsList,
         NewHabitModal,
+        SelfBuildingSquareSpinner,
+    },
+    data() {
+        return {
+            habits: [],
+            loading: {
+                habits: true,
+            },
+        };
+    },
+    methods: {
+        getAllHabitsFromAPI() {
+            // Get all habits from API
+            let component = this;
+            Habit.getAllFromAPI()
+                .then((response) => {
+                    component.habits = response.data.data;
+                })
+                .catch((error) => {
+                    console.log({ error });
+                })
+                .finally(() => {
+                    component.loading.habits = false;
+                });
+        },
+    },
+    beforeMount() {
+        this.getAllHabitsFromAPI();
+
+        // Add event listener para
+        let comp = this;
+        this.$root.$on("new-habit-created", () => {
+            comp.getAllHabitsFromAPI();
+            comp.loading.habits = true;
+        });
     },
 };
 </script>
